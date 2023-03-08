3a948fc2f9d986ca696f07ff6a6cdcc131b433ed
Frans Imanuel <fransimanuel99@gmail.com>
2023-03-08 16:43:33 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,16 +8,31 @@ import (
 	"fmt"
 )
 
-//source encode : https://gist.github.com/yingray/57fdc3264b1927ef0f984b533d63abab
+//source encode : https://gist.github.com/yingray/57fdc3264b1927ef0f984b533d63ababg
 //source decode : https://gist.github.com/awadhwana/9c95377beba61293390c5fd23a3bb1df
 
 func main() {
+	// fmt.Println(string([]byte{byte(12), byte()}))
+	// panic(1)
+
+	// fmt.Println(string([]byte{119, 145, 179, 198, 215, 144, 183, 53, 162, 193, 148, 10, 221, 135, 82, 160, 253, 119, 41, 165, 18, 205, 169, 108, 104, 109, 99, 32, 11, 60, 74, 2, 242, 154, 197, 92, 121, 193, 226, 125, 224, 95, 24, 38, 133, 245,
+	// 	36, 183, 34, 113, 180, 107, 198, 37, 82, 175, 113, 6, 222, 236, 210, 242, 7, 85}))
+	// panic(1)
+
+	// key := "YWRzaGpha2hkc2prYWRoamFza2hkc2pha2Roc2Foamg="
+	// iv := "AAAAAAAAAAAAAAAAAAAAAA=="
+	// plaintext := "abcdefghijklmnopqrstuvwxyzABCDEF"
 	key := "12345678901234567890123456789012"
 	iv := "1234567890123456"
-	plaintext := "abcdefghijklmnopqrstuvwxyzABCDEF"
-	fmt.Printf("Result: %v\n", Ase256(plaintext, key, iv, aes.BlockSize))
+	// plaintext := "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+	// fmt.Printf("Result: %v\n", Ase256(plaintext, key, iv, aes.BlockSize))
 
-	cipherText := "b91d3ece42c203729b38ae004e96efb90109ee25f7861b6bb33891be88d9a7996a5f10bb949360bddd1f7623c15552c4"
+	// key := []byte{119, 145, 179, 198, 215, 144, 183, 53, 162, 193, 148, 10, 221, 135, 82, 160, 253, 119, 41, 165, 18, 205, 169, 108, 104, 109, 99, 32, 11, 60, 74, 2, 242, 154, 197, 92, 121, 193, 226, 125, 224, 95, 24, 38, 133, 245, 36, 183, 34, 113, 180, 107, 198, 37, 82, 175, 113, 6, 222, 236, 210, 242, 7, 85}
+
+	// iv := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	// cipherText := "b91d3ece42c203729b38ae004e96efb90109ee25f7861b6bb33891be88d9a7996a5f10bb949360bddd1f7623c15552c4"
+	// cipherText := "d5GzxteQtzWiwZQK3YdSoP13KaUSzalsaG1jIAs8SgLymsVcecHifeBfGCaF9SS3InG0a8YlUq9xBt7s0vIHVQ=="
+	cipherText := "bc24b69dd978d17bc3bf47b6a447951b1c713de53215ffa34559c20cbdb6554020b9d71639e850a6fed902afc1ed8bb03962591444357ac9d44c458d8f299f57"
 	fmt.Printf("Decode : %v\n", Ase256Decode(cipherText, key, iv))
 }
 
